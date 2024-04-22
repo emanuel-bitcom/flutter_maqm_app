002d5bdc6e3cc3ff39fce136863ed0c883f046d1
emanuel-bitcom <emanuel@bitcom.ro>
2024-04-22 15:53:06 +0300

    Added Dummy Drawer

---
+++ lib/dafaults/defaults.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Defaults {
+  static final Color drawerItemColor = Colors.blueGrey;
+  static final Color? drawerItemSelectedColor = Colors.blue[700];
+
+  static final drawerItemText = [
+    'Home',
+    'Write Date',
+    'Setup LoRa',
+  ];
+
+  static final drawerItemIcon = [
+    Icons.home,
+    Icons.access_time_filled_sharp,
+    Icons.wifi_tethering,
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_nfc_basic_application/dafaults/defaults.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 import 'package:nfc_manager/platform_tags.dart';
 
@@ -9,12 +12,25 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+var indexClicked = 0;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  State<StatefulWidget> createState() => MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
 }
 
-class MyAppState extends State<MyApp> {
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage> {
   ValueNotifier<dynamic> result = ValueNotifier(null);
 
   final TextEditingController yearController = TextEditingController();
@@ -33,103 +49,180 @@ class MyAppState extends State<MyApp> {
   final TextEditingController secondController = TextEditingController();
   String secondLabel = 'second';
 
+  void Function() updateState(int index) {
+    return () {
+      setState(() {
+        indexClicked = index;
+      });
+      Navigator.pop(context);
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('mAQM NFC writer')),
-        body: SafeArea(
-          child: FutureBuilder<bool>(
-            future: NfcManager.instance.isAvailable(),
-            builder: (context, ss) => ss.data != true
-                ? Center(child: Text('NfcManager.isAvailable(): ${ss.data}'))
-                : Flex(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    direction: Axis.vertical,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          margin: EdgeInsets.all(4),
-                          constraints: BoxConstraints.expand(),
-                          decoration: BoxDecoration(border: Border.all()),
-                          child: SingleChildScrollView(
-                            child: ValueListenableBuilder<dynamic>(
-                              valueListenable: result,
-                              builder: (context, value, _) =>
-                                  Text('${value ?? ''}'),
-                            ),
+    return Scaffold(
+      appBar: AppBar(title: Text('mAQM NFC writer')),
+      body: SafeArea(
+        child: FutureBuilder<bool>(
+          future: NfcManager.instance.isAvailable(),
+          builder: (context, ss) => ss.data != true
+              ? Center(child: Text('NfcManager.isAvailable(): ${ss.data}'))
+              : Flex(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  direction: Axis.vertical,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        constraints: BoxConstraints.expand(),
+                        decoration: BoxDecoration(border: Border.all()),
+                        child: SingleChildScrollView(
+                          child: ValueListenableBuilder<dynamic>(
+                            valueListenable: result,
+                            builder: (context, value, _) =>
+                                Text('${value ?? ''}'),
                           ),
                         ),
                       ),
-                      Flexible(
-                        flex: 4,
-                        child: GridView.count(
-                          padding: EdgeInsets.all(4),
-                          crossAxisCount: 2,
-                          childAspectRatio: 4,
-                          crossAxisSpacing: 4,
-                          mainAxisSpacing: 4,
-                          children: [
-                            ElevatedButton(
-                                child: Text('Tag Read'), onPressed: _tagRead),
-                            ElevatedButton(
-                                child: Text('NfcA Read Memory'),
-                                onPressed: _nfcaRead),
-                            CenteredLabel(label: yearLabel),
-                            FlexDateTextField(
-                                numberController: yearController,
-                                label: yearLabel),
-                            CenteredLabel(label: monthLabel),
-                            FlexDateTextField(
-                                numberController: monthController,
-                                label: monthLabel),
-                            CenteredLabel(label: dayLabel),
-                            FlexDateTextField(
-                                numberController: dayController,
-                                label: dayLabel),
-                            CenteredLabel(label: weekdayLabelShort),
-                            FlexDateTextField(
-                                numberController: weekdayController,
-                                label: weekdayLabel),
-                            CenteredLabel(label: hourLabel),
-                            FlexDateTextField(
-                                numberController: hourController,
-                                label: hourLabel),
-                            CenteredLabel(label: minuteLabel),
-                            FlexDateTextField(
-                                numberController: minuteController,
-                                label: minuteLabel),
-                            CenteredLabel(label: secondLabel),
-                            FlexDateTextField(
-                                numberController: secondController,
-                                label: secondLabel),
-                          ],
-                        ),
+                    ),
+                    Flexible(
+                      flex: 4,
+                      child: GridView.count(
+                        padding: EdgeInsets.all(4),
+                        crossAxisCount: 2,
+                        childAspectRatio: 4,
+                        crossAxisSpacing: 4,
+                        mainAxisSpacing: 4,
+                        children: [
+                          ElevatedButton(
+                              child: Text('Tag Read'), onPressed: _tagRead),
+                          ElevatedButton(
+                              child: Text('NfcA Read Memory'),
+                              onPressed: _nfcaRead),
+                          CenteredLabel(label: yearLabel),
+                          FlexDateTextField(
+                              numberController: yearController,
+                              label: yearLabel),
+                          CenteredLabel(label: monthLabel),
+                          FlexDateTextField(
+                              numberController: monthController,
+                              label: monthLabel),
+                          CenteredLabel(label: dayLabel),
+                          FlexDateTextField(
+                              numberController: dayController, label: dayLabel),
+                          CenteredLabel(label: weekdayLabelShort),
+                          FlexDateTextField(
+                              numberController: weekdayController,
+                              label: weekdayLabel),
+                          CenteredLabel(label: hourLabel),
+                          FlexDateTextField(
+                              numberController: hourController,
+                              label: hourLabel),
+                          CenteredLabel(label: minuteLabel),
+                          FlexDateTextField(
+                              numberController: minuteController,
+                              label: minuteLabel),
+                          CenteredLabel(label: secondLabel),
+                          FlexDateTextField(
+                              numberController: secondController,
+                              label: secondLabel),
+                        ],
                       ),
-                      Flexible(
-                        flex: 2,
-                        child: ElevatedButton(
-                          onPressed: _tagFillDate,
-                          child: Text('Fill current date'),
-                        ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: ElevatedButton(
+                        onPressed: _tagFillDate,
+                        child: Text('Fill current date'),
                       ),
-                      Flexible(
-                        flex: 1,
-                        child: SizedBox(
-                          height: 10,
-                        ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: SizedBox(
+                        height: 10,
                       ),
-                      Flexible(
-                        flex: 2,
-                        child: ElevatedButton(
-                          onPressed: _tagWriteDate,
-                          child: Text('Write date'),
-                        ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: ElevatedButton(
+                        onPressed: _tagWriteDate,
+                        child: Text('Write date'),
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/drawer.jpg'),
+                ),
+              ),
+              padding: EdgeInsets.all(0),
+              child: Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage:
+                          AssetImage('assets/images/BitLogoOnly.png'),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'mini Air Quality Monitor',
+                      style: GoogleFonts.sanchez(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Text(
+                      'hello@bitcom.ro',
+                      style: GoogleFonts.sanchez(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  AppDrawerTile(
+                    index: 0,
+                    onTileTap: updateState(0),
                   ),
-          ),
+                  AppDrawerTile(
+                    index: 1,
+                    onTileTap: updateState(1),
+                  ),
+                  AppDrawerTile(
+                    index: 2,
+                    onTileTap: updateState(2),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -164,24 +257,24 @@ class MyAppState extends State<MyApp> {
     });
   }
 
-  void _tagFillDate(){
+  void _tagFillDate() {
     return setState(() {
-    DateTime now = DateTime.now();
-    int year_int = now.year;
-    int month_int = now.month;
-    int day_int = now.day;
-    int weekday_int = now.weekday + 1;
-    if(weekday_int == 8) weekday_int = 1;
-    int hour_int = now.hour;
-    int minute_int = now.minute;
-    int second_int = now.second;
-    yearController.text = year_int.toString();
-    monthController.text = month_int.toString();
-    dayController.text = day_int.toString();
-    weekdayController.text = weekday_int.toString();
-    hourController.text = hour_int.toString();
-    minuteController.text = minute_int.toString();
-    secondController.text = second_int.toString();
+      DateTime now = DateTime.now();
+      int year_int = now.year;
+      int month_int = now.month;
+      int day_int = now.day;
+      int weekday_int = now.weekday + 1;
+      if (weekday_int == 8) weekday_int = 1;
+      int hour_int = now.hour;
+      int minute_int = now.minute;
+      int second_int = now.second;
+      yearController.text = year_int.toString();
+      monthController.text = month_int.toString();
+      dayController.text = day_int.toString();
+      weekdayController.text = weekday_int.toString();
+      hourController.text = hour_int.toString();
+      minuteController.text = minute_int.toString();
+      secondController.text = second_int.toString();
     });
   }
 
@@ -297,6 +390,39 @@ class MyAppState extends State<MyApp> {
       // result.value = ntagA.;
       NfcManager.instance.stopSession();
     });
+  }
+}
+
+class AppDrawerTile extends StatelessWidget {
+  const AppDrawerTile({
+    super.key,
+    required this.index,
+    required this.onTileTap,
+  });
+
+  final int index;
+  final void Function() onTileTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTileTap,
+      leading: Icon(
+        Defaults.drawerItemIcon[index],
+        size: 35,
+        color: indexClicked == index
+            ? Defaults.drawerItemSelectedColor
+            : Defaults.drawerItemColor,
+      ),
+      title: Text(Defaults.drawerItemText[index],
+          style: GoogleFonts.sanchez(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: indexClicked == index
+                ? Defaults.drawerItemSelectedColor
+                : Defaults.drawerItemColor,
+          )),
+    );
   }
 }
 
